d50f440ada945ff72774f7405e2aa68864d5cda1
Adam Brodzinski <adambrodzinski@gmail.com>
2016-06-30 21:29:27 -0700

    adds IO.inspect

--- src/io.js
+++ src/io.js
@@ -7,4 +7,24 @@ export default {
     console.log.apply(console, arguments);
     return "ok";
   },
+
+  // useful for printing out the contents in a pipeline
+  //
+  // example
+  // _.chain([1, 2])
+  //   .map(x => x * 2)
+  //   .thru(IO.inspect)
+  //   .map(x => x * 2)
+  //   .thru(IO.inspect)
+  //   .value();
+  //
+  // "[2, 4]"  (console.logs)
+  // "[6, 8]"
+  // >> [6,8]
+  //
+  // puts(any) :: any
+  inspect(arg1) {
+    console.log(arg1);
+    return arg1;
+  },
 };
--- test/index.js
+++ test/index.js
@@ -9,4 +9,12 @@ describe("IO", () => {
   it("puts should call console.log", () => {
     assert.equal(IO.puts("Hello"), "ok");
   });
+
+  xit("inspect should call console.log and print first argument", () => {
+    // TODO spy on console.log
+  });
+
+  it("inspect should return the argument after printing", () => {
+    assert.equal(IO.inspect(3), 3);
+  });
 });
